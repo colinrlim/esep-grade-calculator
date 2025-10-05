7023d007b8703e33d2eee24a5ac20c0ff51c36fc
TheHarryl <44008906+TheHarryl@users.noreply.github.com>
2025-10-04 23:55:01 -0400

    Bonus 1: Refactor to use single grades list instead of separate category lists

--- grade-calculator/grade_calculator.go
+++ grade-calculator/grade_calculator.go
@@ -1,9 +1,7 @@
 package esepunittests
 
 type GradeCalculator struct {
-	assignments []Grade
-	exams       []Grade
-	essays      []Grade
+	grades []Grade
 }
 
 type GradeType int
@@ -32,9 +30,7 @@ type Grade struct {
 
 func NewGradeCalculator() *GradeCalculator {
 	return &GradeCalculator{
-		assignments: make([]Grade, 0),
-		exams:       make([]Grade, 0),
-		essays:      make([]Grade, 0),
+		grades: make([]Grade, 0),
 	}
 }
 
@@ -55,34 +51,32 @@ func (gc *GradeCalculator) GetFinalGrade() string {
 }
 
 func (gc *GradeCalculator) AddGrade(name string, grade int, gradeType GradeType) {
-	switch gradeType {
-	case Assignment:
-		gc.assignments = append(gc.assignments, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Assignment,
-		})
-	case Exam:
-		gc.exams = append(gc.exams, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Exam,
-		})
-	case Essay:
-		gc.essays = append(gc.essays, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Essay,
-		})
-	}
+	gc.grades = append(gc.grades, Grade{
+		Name:  name,
+		Grade: grade,
+		Type:  gradeType,
+	})
 }
 
 func (gc *GradeCalculator) calculateNumericalGrade() int {
-	assignment_average := computeAverage(gc.assignments)
-	exam_average := computeAverage(gc.exams)
-	essay_average := computeAverage(gc.essays)
+	var assignments, exams, essays []Grade
+	
+	for _, grade := range gc.grades {
+		switch grade.Type {
+		case Assignment:
+			assignments = append(assignments, grade)
+		case Exam:
+			exams = append(exams, grade)
+		case Essay:
+			essays = append(essays, grade)
+		}
+	}
+	
+	assignment_average := computeAverage(assignments)
+	exam_average := computeAverage(exams)
+	essay_average := computeAverage(essays)
 
-	weighted_grade := float64(assignment_average)*.5 + float64(exam_average)*.35 + float64(essay_average)*.15
+	weighted_grade := float64(assignment_average)*0.5 + float64(exam_average)*0.35 + float64(essay_average)*0.15
 
 	return int(weighted_grade)
 }
